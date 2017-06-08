4a1366ec09922c106e506105bb871fd922798798
Quang Tran <quangtt@rikkeisoft.com>
2017-06-08 13:49:59 +0700

    Auto-Caption for post image

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,3 +67,18 @@ function extractDomain(url) {
   domain = domain.replace(".com", "");
   return domain;
 }
+
+// Creates Captions from Alt tags
+$(".post-content img").each(function() {
+  // Let's put a caption if there is one
+  if ($(this).attr("alt")) {
+    $(this).wrap(
+      '<figure class="image"></figure>'
+    ).after(
+      '<figcaption>' +
+      $(this).attr(
+          "alt") +
+      '</figcaption>'
+    );
+  }
+});
