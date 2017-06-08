0d67c3ca1dd1dcd448c81aec2a431289dc137875
Quang Tran <quangtt@rikkeisoft.com>
2017-06-08 11:21:20 +0700

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+  // Loading
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  Pace.on('done', function(event) {
+  });
+
+  // Animations
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  new WOW().init();
+
+  // Responsive videos
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  $(".site-main").fitVids();
+
+  // Navigation toggle
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  $('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+    $('body').toggleClass('no-scroll');
+  });
+
+  // Social icons
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  if ( typeof social_icons !== 'undefined' ) {
+    $.each( social_icons, function(index, val) {
+       $(".social-navigation ul").append('<li><a href="' + val + '">' + extractDomain(val) + '</a></li>');
+    });
+  } else {
+    $(".social-navigation").remove();
+  }
+
+  // Scroll to top
+  // ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  $('.scroll-top').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
+
+});
+
+// Extract domain
+// ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+function extractDomain(url) {
+  var domain;
+
+  // Find & remove protocol (http, ftp, etc.) and get domain
+  if (url.indexOf("://") > -1) {
+    domain = url.split('/')[2];
+  } else {
+    domain = url.split('/')[0];
+  }
+
+  // Find & remove port number
+  domain = domain.split(':')[0];
+  domain = domain.replace("www.", "");
+  domain = domain.replace(".com", "");
+  return domain;
+}
